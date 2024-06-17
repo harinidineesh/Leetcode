ca29e4207512de42a08eb73b92074a54b6733d5b
HARINI <112502661+harinidineesh@users.noreply.github.com>
2024-06-17 09:00:01 +0530

    Time: 4 ms (31.17%), Space: 5.5 MB (35.07%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.c
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.c
@@ -1,8 +1,6 @@
 bool judgeSquareSum(int c) {
     for (long a = 0; a * a <= c; ++a) {
-        // Calculate b^2 = c - a^2
         long b = c - a * a;
-        // Check if b is a perfect square
         long b_sqrt = (long)sqrt(b);
         if (b_sqrt * b_sqrt == b) {
             return true;
