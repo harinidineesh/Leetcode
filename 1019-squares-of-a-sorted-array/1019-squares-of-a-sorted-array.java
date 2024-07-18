011f8ba096c3b3a4d68370143e763f783d7aec0e
HARINI <112502661+harinidineesh@users.noreply.github.com>
2024-07-18 19:30:30 +0530

    Time: 1 ms (100%), Space: 46.7 MB (69.17%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,17 +1,33 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]= nums[i]*nums[i];
-        }
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]>nums[j]){
-                    int temp = nums[i];
-                    nums[i]= nums[j];
-                    nums[j] = temp;
-                }
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i]= nums[i]*nums[i];
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]>nums[j]){
+        //             int temp = nums[i];
+        //             nums[i]= nums[j];
+        //             nums[j] = temp;
+        //         }
+        //     }
+        // }
+        // return nums;
+
+        int left=0, right = nums.length-1, pointer = nums.length-1;
+        int []result = new int [nums.length];
+        while(left<=right){
+            if(Math.abs(nums[left])> Math.abs(nums[right])){
+                result[pointer]= nums[left]*nums[left];
+                left++;
+                pointer--;
+            }
+            else{
+                result[pointer]= nums[right]*nums[right];
+                right--;
+                pointer--;
             }
         }
-        return nums;
+        return result;
     }
 }
